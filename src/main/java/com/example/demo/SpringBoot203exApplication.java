29bbd7fa9b2b6ab5171a9b0f38468741a13b3047
GBTC405007UR <GBTC405007UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-20 13:55:39 -0400

    203 lesson

---
+++ src/main/java/com/example/demo/SpringBoot203exApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot203exApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot203exApplication.class, args);
+    }
+
+}
